e9a33173c6c1b0337028e89e6897490a76dd79d5
Paul Cushing <pcushing@gmail.com>
2024-01-31 17:21:41 -0700

    Security updates

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { sentrySvelteKit } from '@sentry/sveltekit';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'gem-state-media',
-				project: 'whatgodsaysaboutme',
-				authToken: process.env.SENTRY_AUTH_TOKEN
-			}
-		}),
 		sveltekit()
 	]
 };
